d043236fb16d8e06dd6794ec2f8de258b1d5e0a3
Gregorio Granado Magalhaes <greg.magalhaes@gmail.com>
2020-05-06 14:37:20 -0300

    added tab bar and some small fixes

---
+++ flyline_flutter_app-master/lib/modules/menuitems/menu_item_tab_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class MenuItemTabBar extends StatelessWidget {
+  const MenuItemTabBar({
+    Key key,
+    @required this.tabs,
+  }) : super(key: key);
+
+  final List<String> tabs;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      child: TabBar(
+        indicatorColor: Colors.transparent,
+        labelColor: Color(0xFF0E3178),
+        unselectedLabelColor: Color(0xFF0E3178).withOpacity(.5),
+        labelStyle: TextStyle(
+          fontFamily: 'Gilroy',
+          fontWeight: FontWeight.bold,
+          fontSize: 14,
+          height: 2,
+        ),
+        unselectedLabelStyle: TextStyle(
+          fontFamily: 'Gilroy',
+          fontWeight: FontWeight.bold,
+          fontSize: 14,
+          height: 2,
+        ),
+        tabs: [
+          ...tabs.map((t) => t.length != tabs.indexOf(t)
+              ? Container(
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    border: Border(
+                      right: BorderSide(
+                        color: Color.fromRGBO(231, 233, 240, 1),
+                        width: 1,
+                        style: BorderStyle.solid,
+                      ),
+                    ),
+                  ),
+                  child: Tab(
+                    child: Text(t),
+                  ),
+                )
+              : Tab(
+                  child: Text(t),
+                )),
+        ],
+      ),
+    );
+  }
+}
